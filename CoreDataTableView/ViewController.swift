46d72013f12697c56281121090bc6cde6cfb03c4
gdsanders <gd@digitalequity.com>
2015-10-16 11:28:19 -0500

    Setting up tableView in code

--- CoreDataTableView/ViewController.swift
+++ CoreDataTableView/ViewController.swift
@@ -9,15 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var names = [String]()
+    let tableView = UITableView()
+    var newNameInput: UITextField?
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.\
+        
+        title = "Users"
+        edgesForExtendedLayout = UIRectEdge.None
+        tableView.frame = view.frame
+        
+        view.addSubview(tableView)
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain, target: self, action: Selector ("toggleEdit"))
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        navigationItem.rightBarButtonItem = addButton
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func toggleEdit() {
+        tableView.setEditing(!tableView.editing, animated: true)
     }
 
 
